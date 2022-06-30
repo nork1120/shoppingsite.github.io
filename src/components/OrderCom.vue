7fd20007542f6016b442b33a09c5a9c5858d7aaf
nork1120 <nork1120@gmail.com>
2022-06-30 19:58:26 +0800

    最新

---
+++ src/components/OrderCom.vue
@@ -0,0 +1,19 @@
+<template>
+        <h1>ss</h1>
+        <h1>ss</h1>
+        <h1>ss</h1>
+        <h1>ss</h1>
+        <h1>ss</h1>
+        <h1>ss</h1>
+        <h1>ss</h1>
+        <h1>ss</h1>
+        <h1>ss</h1>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped lang="scss">
+    
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    component:()=>import('../views/HomeView.vue')
+    component: () => import('../views/HomeView.vue')
   },
   {
     path: '/about',
@@ -13,34 +13,42 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },{
-    path:'/Login',
-    name:'Login',
-    component:()=>import('../views/LoginView.vue')
-  },{
-    path:'/LoginOK',
-    name:'LoginOK',
-    component:()=>import('../views/LoginOK.vue'),
-    children:[{
-      path:'ObtAin',
-      component:()=>import('../views/ObtAin.vue')
+  }, {
+    path: '/Login',
+    name: 'Login',
+    component: () => import('../views/LoginView.vue')
+  }, {
+    path: '/LoginOK',
+    name: 'LoginOK',
+    component: () => import('../views/LoginOK.vue'),
+    children: [{
+      path: 'ObtAin',
+      component: () => import('../views/ObtAin.vue')
     }]
-  },{
-    path:'/seafishing',
-    name:'seafishing',
-    component:()=>import('../views/SeaFishing.vue')
-  },{
-    path:'/CommoDity',
-    name:'CommoDity',
-    component:()=>import('../views/CommoDity.vue')
-  },{
-    path:'/gobay',
-    name:'gobay',
-    component:()=>import('../views/GoBuy.vue'),
-  },{
-    path:'/checkout',
-    name:'checkout',
-    component:()=>import('../views/CheckOut.vue')
+  }, {
+    path: '/seafishing',
+    name: 'seafishing',
+    component: () => import('../views/SeaFishing.vue')
+  }, {
+    path: '/CommoDity',
+    name: 'CommoDity',
+    component: () => import('../views/CommoDity.vue')
+  }, {
+    path: '/gobay',
+    name: 'gobay',
+    component: () => import('../views/GoBuy.vue'),
+  }, {
+    path: '/checkout',
+    name: 'checkout',
+    component: () => import('../views/CheckOut.vue'),
+  }, {
+    path: '/confirm',
+    name: 'confirm',
+    component: () => import('../views/ConFirm.vue'),
+    children: [{
+      path: 'order',
+      component:()=>import('../components/OrderCom.vue')
+    }]
   }
 ]
 
